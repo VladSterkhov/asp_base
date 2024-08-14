bf1ef4dddd383264cf5ab77ed7f9933db8c8586b
VladSterkhov <vswork2105@gmail.com>
2024-08-14 21:53:50 +0300

    test ssh_key

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddTimeService();
+        builder.Services.AddTimeService(); 
         builder.Services.AddLogger();
 
         var app = builder.Build();
